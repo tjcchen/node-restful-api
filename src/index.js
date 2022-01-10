c14dd732efae5bb5214ecd33704bd59c97b66bb9
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-10 23:00:59 +0800

    feat: log each request path

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@
  */
 
 // Dependencies
-var http = require('http');
+const http = require('http');
+const url  = require('url');
 
 // The server should respond to all requests with a string
-var server = http.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
+  // 1. Get the url and parse it
+  let parsedUrl = url.parse(req.url, true);
+
+  // 2. Get the path
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // 3. Send the response
   res.end('Hello Nodejs Restful API!\n');
+
+  // 4. Log the request path
+  console.log('Request received on path:', trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
