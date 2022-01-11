57b12c640fc8c507b3cd22b2cd7c4ae7adaab5be
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-11 22:53:41 +0800

    feat: Get the query string as an object

--- src/index.js
+++ src/index.js
@@ -8,21 +8,25 @@ const url  = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
-  // 1. Get the url and parse it
+  // Get the url and parse it
   let parsedUrl = url.parse(req.url, true);
 
-  // 2. Get the path
+  // Get the path
   let path = parsedUrl.pathname;
   let trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  // 3. Get the HTTP Method
+  // Get the query string as an object
+  let queryStringObject = parsedUrl.query;
+
+  // Get the HTTP Method
   let method = req?.method?.toUpperCase();
 
-  // 4. Send the response
+  // Send the response
   res.end('Hello Nodejs Restful API!\n');
 
-  // 5. Log the request path
+  // Log the request path
   console.log('Request received on path:', trimmedPath, '; with method', method);
+  console.log(JSON.stringify(queryStringObject, null, 4));
 });
 
 // Start the server, and have it listen on port 3000
