096ae71dc398480abf8cadb5aadf4cb1fc12a141
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-13 22:41:54 +0800

    feat: get request headers

--- src/index.js
+++ src/index.js
@@ -21,12 +21,16 @@ const server = http.createServer(function(req, res) {
   // Get the HTTP Method
   let method = req?.method?.toUpperCase();
 
+  // Get the headers as an object
+  let headers = req.headers;
+
   // Send the response
   res.end('Hello Nodejs Restful API!\n');
 
   // Log the request path
-  console.log('Request received on path:', trimmedPath, '; with method', method);
-  console.log(JSON.stringify(queryStringObject, null, 4));
+  // console.log('Request received on path:', trimmedPath, '; with method', method);
+  // console.log(JSON.stringify(queryStringObject, null, 4));
+  console.log('Request received with these headers: ', headers);
 });
 
 // Start the server, and have it listen on port 3000
