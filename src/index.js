ea5966fc12f1d93f8539d908119b3ecb09b8d18d
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-13 23:06:20 +0800

    feat: parsing the payload

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require('http');
 const url  = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
@@ -24,16 +25,29 @@ const server = http.createServer(function(req, res) {
   // Get the headers as an object
   let headers = req.headers;
 
+  // Get the payload, if any
+  // Only the POST request contains a payload, GET request does not have
+  let decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+      buffer += decoder.write(data); // decode the payload data
+  });
+  req.on('end', () => {
+      buffer += decoder.end();
+      res.end('Hello Nodejs Restful API!\n');
+      console.log('Request received with this payload: ', buffer);
+  });
+
   // Send the response
-  res.end('Hello Nodejs Restful API!\n');
+  // res.end('Hello Nodejs Restful API!\n');
 
   // Log the request path
   // console.log('Request received on path:', trimmedPath, '; with method', method);
   // console.log(JSON.stringify(queryStringObject, null, 4));
-  console.log('Request received with these headers: ', headers);
+  // console.log('Request received with these headers: ', headers);
 });
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, function() {
-  console.log('The server is listening on port 3000 now');
+    console.log('The server is listening on port 3000 now');
 });
