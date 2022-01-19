2e710def5da6770f6da6845e5ea5b42bf3e5f7a5
Andy Chen <chenyangpoppin@hotmail.com>
2022-01-19 22:45:42 +0800

    chore: update http and https port config

--- src/config.js
+++ src/config.js
@@ -7,13 +7,15 @@ let environments = {};
 
 // Staging( default ) environment
 environments.staging = {
-    port: 3000,
+    httpPort: 3000,
+    httpsPort: 3001,
     envName: 'staging'
 };
 
 // Production environment
 environments.production = {
-    port: 5000,
+    httpPort: 5000,
+    httpsPort: 5001,
     envName: 'production'
 };
 
