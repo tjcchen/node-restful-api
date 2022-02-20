5977a30a2aef56c18b09a40581cc6859faef2da7
Andy Chen <chenyangpoppin@hotmail.com>
2022-02-20 10:44:59 +0800

    feat: add checks service root api

--- src/index.js
+++ src/index.js
@@ -34,7 +34,8 @@ let router = {
     'sample': handlers.sample,
     'ping': handlers.ping,
     'users': handlers.users,
-    'tokens': handlers.tokens
+    'tokens': handlers.tokens,
+    'checks': handlers.checks 
 };
 
 // Instantiate the HTTP server
