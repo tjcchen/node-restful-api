5a396dd0e3ebe8d3a41e56d59714a441262aa0d4
Andy Chen <chenyangpoppin@hotmail.com>
2022-04-13 23:37:04 +0800

    feat: add making request url functionality

--- public/app.js
+++ public/app.js
@@ -25,15 +25,56 @@ app.client.request = (headers, path, method, queryStringObject, payload, callbac
 
   // For each query string parameter sent, add it to the path
   let requestUrl = path + '?';
-  let count = 0;
+  let counter = 0;
   for (let queryKey in queryStringObject) {
     if (queryStringObject.hasOwnProperty(queryKey)) {
-      count++;
-
+      counter++;
       // If at least one query string parameter has already been added, prepend new ones with an ampersand
-      
+      if (counter > 1) {
+        requestUrl += '&';
+      }
+      // Add the key and value
+      requestUrl += queryKey + '=' + queryStringObject[queryKey];
     }
   }
 
+  // Form the http request as a JSON type
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, requestUrl, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+
+  // For each header sent, add it to the request
+  for (let headerKey in headers) {
+    if (headers.hasOwnProperty(headerKey)) {
+      xhr.setRequestHeader(headerKey, headers[headerKey]);
+    }
+  }
+
+  // If there is a current session token set, add that as a header
+  if (app.config.sessionToken) {
+    xhr.setRequestHeader('token', app.config.sessionToken.id);
+  }
+
+  // when the request comes back, handle the response
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      let statusCode = xhr.status;
+      let responseReturned = xhr.responseText;
+
+      // Callback if requested
+      if (callback) {
+        try {
+          let parsedResponse = JSON.parse(responseReturned);
+          callback(statusCode, parsedResponse);
+        } catch(e) {
+          callback(statusCode, false);
+        }
+      }
+    }
+  };
+
+  // Send the payload as JSON
+  let payloadString = JSON.stringify(payload);
+  xhr.send(payloadString);
 };
 
