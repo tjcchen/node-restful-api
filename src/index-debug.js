9540a2ab32677bcd4105ec9547a76ad335f35336
Andy Chen <chenyangpoppin@hotmail.com>
2022-05-12 23:03:37 +0800

    feat: add debugging code logic

---
+++ src/index-debug.js
@@ -0,0 +1,31 @@
+/**
+ * Primary file for the API
+ */
+
+// Dependencies
+const server = require('../lib/server');
+const workers = require('../lib/workers');
+const cli = require('../lib/cli');
+
+// Declare the app
+const app = {};
+
+// Init function
+app.init = () => {
+    // Start the server
+    server.init();
+
+    // Start the workers
+    workers.init();
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(() => {
+        cli.init();
+    }, 50);
+};
+
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
