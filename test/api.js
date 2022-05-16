203ea79a3708dcac57e7b6ed7996a3e4fd998ba4
Andy Chen <chenyangpoppin@hotmail.com>
2022-05-16 23:13:24 +0800

    feat: add helper scaffolding

--- test/api.js
+++ test/api.js
@@ -11,6 +11,17 @@ const config = require('../lib/config');
 // Holder for the tests
 const api = {};
 
+// Helpers
+const helpers = {};
+
+helpers.makeGetRequest = (path, callback) => {
+    // Configure the request details
+
+
+    // Send the request
+    
+};
+
 
 // Export the tests to the runner
 module.exports = api;
