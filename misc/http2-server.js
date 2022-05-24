742161454a3070623e075816d67c9ce65931c6e0
Andy Chen <chenyangpoppin@hotmail.com>
2022-05-24 23:32:57 +0800

    feat: adding http server side of logic

--- misc/http2-server.js
+++ misc/http2-server.js
@@ -1,3 +1,21 @@
-// Http2 server side
-//
-// 
+/**
+ * Example HTTP2 Server
+ */
+
+// Dependencies
+const http2 = require('http2');
+
+// Init the server
+const server = http2.createServer();
+
+// On a stream, send back hello world html
+server.on('stream', (stream, headers) => {
+    stream.respond({
+        'status': 200,
+        'content-type': 'text/html'
+    });
+    stream.end('<html><body><p>Hello World!</p></body></html>');
+});
+
+// Listen on 6000
+server.listen(6000);
