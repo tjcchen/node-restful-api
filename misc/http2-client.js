04c79dd73331100d0d1bda672ac65e8e240b0499
Andy Chen <chenyangpoppin@hotmail.com>
2022-05-25 23:00:15 +0800

    feat: add the main logic of http2 client side

--- misc/http2-client.js
+++ misc/http2-client.js
@@ -1,3 +1,28 @@
 /**
  * Example HTTP2 Client
  */
+
+// Dependencies
+const http2 = require('http2');
+
+// Create client
+const client = http2.connect('http://localhost:6000');
+
+// Create a request
+const req = client.request({
+    ':path': '/'
+});
+
+// When a message is received, and the process of it together until you reach the end
+let str = '';
+req.on('data', (chunk) => {
+    str += chunk;
+});
+
+// When the message ends, log it out
+req.on('end', () => {
+    console.log(str);
+});
+
+// End the request
+req.end();
