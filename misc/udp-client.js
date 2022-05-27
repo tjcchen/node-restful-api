3fa8ec3ead1ae444fe71d45ce88cd25b7f8af1d7
Andy Chen <chenyangpoppin@hotmail.com>
2022-05-27 22:40:58 +0800

    feat: adding udp client side logic

---
+++ misc/udp-client.js
@@ -0,0 +1,21 @@
+/**
+ * Example UDP client
+ * 
+ * Sending a message to a UDP server on port 6000
+ * 
+ */
+
+// Dependencies
+const dgram = require('dgram');
+
+// Create the client
+const client = dgram.createSocket('udp4');
+
+// Define the message and pull it into a buffer
+let messageString = 'This is a message';
+let messageBuffer = Buffer.from(messageString);
+
+// Send off the message
+client.send(messageBuffer, 6000, 'localhost', (err) => {
+    client.close();
+});
