b9d8f07b6afcc3628cb1efa8e2e0b624dde988ed
Andy Chen <chenyangpoppin@hotmail.com>
2022-05-29 21:03:13 +0800

    feat: adding TLS client side of code logic

---
+++ misc/tls-client.js
@@ -0,0 +1,33 @@
+/**
+ * Example TLS( Transport Layer Security ) Client
+ * 
+ * Connects to port 6000 and sends the word "ping" to server
+ * 
+ */
+
+// Dependencies
+const tls = require('tls');
+const fs = require('fs');
+const path = require('path');
+
+// Client Options
+const options = {
+    // Only required because we're using a self-signed certificate
+    'ca': fs.readFileSync(path.join(__dirname, '../https/cert.pem'))
+};
+
+// Define the message to send
+const outboundMessage = 'ping';
+
+// Create the client
+const client = tls.connect(6000, options, () => {
+    // Send the message
+    client.write(outboundMessage);
+});
+
+// When the server writes back, log what is says then kill the application
+client.on('data', (inboundMessage) => {
+    let messageString = inboundMessage.toString();
+    console.log(`I wrote ${outboundMessage} and they said ${messageString}`);
+    client.end();
+});
